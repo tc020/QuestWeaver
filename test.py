650ed864333004c3cf543d4a91df0504f9e810ee
tc020 <99870964+tc020@users.noreply.github.com>
2024-09-03 20:11:29 +0200

    test anlauf

--- test.py
+++ test.py
@@ -1,1 +1,40 @@
-print("Test")
+#Du hast die Rolle als Spielleiter in der Fanatsiewelt Aventurien aus dem Rollenspielsystem Das Schwarze Auge. Beschreibe eine Taverne mit 1 bis 2 Wirten und mindestens 3 Gästen. Beschreibe jede Figur in 5 Stichpunkten und gebe jeder Figur einen Hintergrund. Gebe mindestens zwei Figuren einen Plot, an den die Helden anknüpfen können, wenn sie sie ansprechen.
+
+#Beschreibe den Plot von Gast 1 detaillierter und füge Elemente hinzu, die für die Helden eine Herausforderung darstellen. 
+
+#Beschreibe Punkt 6 detaillierter und gebe konkrete Beispiele für die Rätsel.
+
+#Modifiziere deine Antwort mit mehr Gefahr und Dark Fantasy Elementen
+
+#Gebe weitere Möglichkeiten mit denen die Helden bei Morwenna auf weitere Abenteuer gehen könnten.
+
+from langchain.output_parsers import ResponseSchema
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+import datetime
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+llm_model = "gpt-4"
+
+
+def get_completion(prompt, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
+
+prompt1 = """
+Erzähle einen Witz in 10 Wörtern
+"""
+
+witz = get_completion(prompt1)
+
+print(witz)
